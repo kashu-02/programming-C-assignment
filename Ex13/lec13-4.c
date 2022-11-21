63a8554852abcf982dd5a4cdf12efd16b911a661
“kashu-02” <64694079+kashu-02@users.noreply.github.com>
2022-11-21 11:22:16 +0900

    ADD prog03b.c

---
+++ Ex13/lec13-4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define WORDS 5
+
+int main()
+{
+  int *arr[WORDS],i,j,word_length;
+  char word[100];
+
+  for(i = 0; i < WORDS; i++){
+    printf("Input word #%d: ", i + 1); 
+    scanf("%s",&word);
+    arr[i] = (*char) malloc(sizeof(char) * strlen(word));
+    strcpy(arr[i], word);
+  }
+
+  for(i = 0; i < WORDS; i++){
+    printf("Word #%d (Length: %d): %s\n", i+1, strlen(arr[i]), arr[i]);
+  }
+  
+  for(i = 0; i < WORDS; i++) free(arr[i]);
+
+  return 0;
+}
